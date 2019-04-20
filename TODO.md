e326aede4fbed343d428b0eee0a519d367e4a5a4
Etienne Schmitz <hello@etienne-schmitz.com>
2019-04-20 19:09:46 +0200

    feat(parser) : Abstract syntax tree - If Then Else
    Add the If/Then/Else statement to the abstract tree.
    Add Minus Expression to the abstract tree.
    Add Not Expression to the abstract tree.
    Modify NodeRel : add unary constructor.

--- TODO.md
+++ TODO.md
@@ -2,3 +2,5 @@
 
 - Erreur lorsque le type n'est pas trouvé. (Null Pointer Exception pour le moment)
 - Ajouter les structure de pointeurs (TYPE_DOT).
+- Problème avec la valeur pour les Nodes id
+- Double définition de variable (Erreur).
--- src/ubordeaux/deptinfo/compilation/project/node/NodeRel.java
+++ src/ubordeaux/deptinfo/compilation/project/node/NodeRel.java
@@ -15,6 +15,13 @@ public class NodeRel extends NodeExp {
 		this.type = new TypeBoolean();
 	}
 
+	//Relation unaire ("not")
+	public NodeRel(String name,Node op){
+		super(op);
+		this.name = name;
+		this.type= new TypeBoolean();
+	}
+
 	@Override
 	public boolean checksType() {
 		super.checksType();
