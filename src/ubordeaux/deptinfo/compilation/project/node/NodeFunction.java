fd4bc6c99d9359e6c79f40ed7f1394aca10b5345
Valentin Gaisset <valentin.gaisset@etu.u-bordeaux.fr>
2019-05-02 13:21:52 +0200

    Add readln / println

--- src/ubordeaux/deptinfo/compilation/project/node/NodeFunction.java
+++ src/ubordeaux/deptinfo/compilation/project/node/NodeFunction.java
@@ -44,4 +44,12 @@ public class NodeFunction extends Node {
 	public String toString() {
 		return "NodeFunction(" + getDeclaration().getName() + ", " + (isDefined() ? getDefinition().toString() : "Not defined");
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((funcDecl == null) ? 0 : funcDecl.hashCode());
+		return result;
+	}
 }
